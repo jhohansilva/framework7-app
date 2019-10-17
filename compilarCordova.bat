a6fa267b7029e8469834069bf590db3d6afa7aaf
Jhohan Silva <jhohanf.silva@gmail.com>
2019-10-17 17:34:35 -0500

    Update package

--- compilarCordova.bat
+++ compilarCordova.bat
@@ -2,11 +2,11 @@
 
 IF ERRORLEVEL 2 GOTO ERROR
 
-xcopy C:\xampp\htdocs\my-app\www "C:\Users\Jhohan\my-app\www" /E /I /Y /exclude:
+xcopy C:\xampp\htdocs\my-app\www "C:\Users\Jhohan\f7App\www" /E /I /Y /exclude:
 echo Proceso Completado
 
-cd "C:\Users\Jhohan\my-app"
-cordova run android
+cd "C:\Users\Jhohan\f7App"
+phonegap serve
 
 pause
 exit
